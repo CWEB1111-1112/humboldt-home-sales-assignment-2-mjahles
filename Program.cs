5cac08d8e691bf7339f8231f99e99a7be09aa93a
mjahles <ahlmatj@dunwoody.edu>
2020-05-11 11:20:02 -0500

    added in a import statement for System.Collections.Generic and added in basic declarations for the program class

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace assignment_four
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DECLARATIONS
+            int attendantId;
+            string attendantName;
+            int dogId;
+            string dogName;
+            int timeWithDog;
+            char services;
+            string dogNote;
+        }
+    }
+    class Dog
+    {
+
+    }
+}
