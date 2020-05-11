4d09fc6b09d30eb358bad362942e1f289db5c23f
mjahles <ahlmatj@dunwoody.edu>
2020-05-11 11:33:02 -0500

    added in encapsulated variables to be used in the log objects. Added in the Log constructor for the log object

--- Program.cs
+++ Program.cs
@@ -19,6 +19,23 @@ namespace assignment_four
     }
     class Dog
     {
+        public int attendantId {get;set;}
+        public string attendantName {get; set;}
+        public int wooferId {get; set;}
+        public string wooferName {get; set;}
+        public int timeWithWoofer {get; set;}
+        public char services {get; set;}
+        public string wooferNote {get; set;}
 
+        public Log(int attendantId, string attendantName, int wooferId, string wooferName, int timeWithWoofer, char services, string wooferNote)
+        {
+            this.attendantId = attendantId;
+            this.attendantName = attendantName;
+            this.wooferId = wooferId;
+            this.wooferName = wooferName;
+            this.timeWithWoofer = timeWithWoofer;
+            this.services = services;
+            this.wooferNote = wooferNote;
+        }
     }
 }
